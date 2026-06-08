575dccf5f290c79d3fd53dbb52b04d7ea33dfa3d
RedVelvetCheesecake <robertnicole.balintec@evsu.edu.ph>
2026-06-09 02:23:03 +0800

    Fix?

--- photo_album_project/settings.py
+++ photo_album_project/settings.py
@@ -154,3 +154,17 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
--- photo_album_project/urls.py
+++ photo_album_project/urls.py
@@ -1,8 +1,16 @@
+import sys
+print("DEBUG: urls.py loading", file=sys.stderr)
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+
+def test_view(request):
+    return HttpResponse("Test OK")
+
 
 def signup(request):
     if request.method == 'POST':
@@ -15,9 +23,12 @@ def signup(request):
     return render(request, 'gallery/signup.html', {'form': form})
 
 urlpatterns = [
+    path('test/', test_view, name='test'),
     path('admin/', admin.site.urls),
     path('', include('gallery.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='gallery/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', signup, name='signup'),
 ]
+
+print("DEBUG: urls.py loaded successfully", file=sys.stderr)
