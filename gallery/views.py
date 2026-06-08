7d49b1acf2c764a821dfaf7beb085dbe6d40877b
RedVelvetCheesecake <robertnicole.balintec@evsu.edu.ph>
2026-06-09 02:53:23 +0800

    Finally fixed

--- gallery/views.py
+++ gallery/views.py
@@ -19,7 +19,7 @@ class AlbumCreateView(LoginRequiredMixin, CreateView):
     model = Album
     form_class = AlbumForm
     template_name = 'gallery/album_form.html'
-    success_url = reverse_lazy('album_list')
+    success_url = reverse_lazy('gallery:album_list')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -34,12 +34,12 @@ class AlbumUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return self.get_object().owner == self.request.user
 
     def get_success_url(self):
-        return reverse_lazy('album_detail', kwargs={'pk': self.object.pk})
+        return reverse_lazy('gallery:album_detail', kwargs={'pk': self.object.pk})
 
 class AlbumDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Album
     template_name = 'gallery/album_confirm_delete.html'
-    success_url = reverse_lazy('album_list')
+    success_url = reverse_lazy('gallery:album_list')
 
     def test_func(self):
         return self.get_object().owner == self.request.user
@@ -52,7 +52,7 @@ class PhotoCreateView(LoginRequiredMixin, CreateView):
     def dispatch(self, request, *args, **kwargs):
         self.album = get_object_or_404(Album, pk=kwargs['album_pk'])
         if self.album.owner != request.user:
-            return redirect('album_detail', pk=self.album.pk)
+            return redirect('gallery:album_detail', pk=self.album.pk)
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
@@ -66,7 +66,7 @@ class PhotoCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('album_detail', kwargs={'pk': self.album.pk})
+        return reverse_lazy('gallery:album_detail', kwargs={'pk': self.album.pk})
 
 class PhotoUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Photo
@@ -77,7 +77,7 @@ class PhotoUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return self.get_object().album.owner == self.request.user
 
     def get_success_url(self):
-        return reverse_lazy('album_detail', kwargs={'pk': self.object.album.pk})
+        return reverse_lazy('gallery:album_detail', kwargs={'pk': self.object.album.pk})
 
 class PhotoDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Photo
@@ -87,4 +87,4 @@ class PhotoDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return self.get_object().album.owner == self.request.user
 
     def get_success_url(self):
-        return reverse_lazy('album_detail', kwargs={'pk': self.object.album.pk})
+        return reverse_lazy('gallery:album_detail', kwargs={'pk': self.object.album.pk})
